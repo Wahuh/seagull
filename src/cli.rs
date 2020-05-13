2223299a9c8c5279c70b4a65e2d5f6258b7641e1
Thanh Doan <tmdoan98@gmail.com>
2020-05-13 23:01:15 +0100

    Add migration history tracking

--- src/cli.rs
+++ src/cli.rs
@@ -34,13 +34,51 @@ pub fn handle_migrate() -> Result<()> {
 
     let connection_string = config.postgres.connection_string();
     let mut client = Client::connect(&connection_string, NoTls)?;
+
+    client
+        .batch_execute("CREATE TABLE IF NOT EXISTS __migration_history ( version_number INTEGER )")
+        .with_context(|| "Failed to create __migration_history table")?;
+
+    let row = client
+        .query_one("SELECT version_number from __migration_history", &[])
+        .ok();
+
+    let current_version_number: i32 = match row {
+        Some(row) => row.get("version_number"),
+        None => 0,
+    };
+
     client.batch_execute("BEGIN")?;
-    for migration in migrations {
+
+    let mut unapplied_migrations: Vec<_> = migrations
+        .iter()
+        .filter(|migration| migration.version_number > current_version_number)
+        .collect();
+
+    // make sure the migrations are in ascending order by version number
+    unapplied_migrations.sort_by_key(|m| m.version_number);
+
+    // get existing migration name
+    for migration in unapplied_migrations {
         println!("{}", &migration.sql);
+
         client.batch_execute(&migration.sql).with_context(|| {
             format!("Failed to run migration for {}", &migration.path.display())
         })?;
+
+        client
+            .execute(
+                "INSERT INTO __migration_history (version_number) VALUES ($1)",
+                &[&migration.version_number],
+            )
+            .with_context(|| {
+                format!(
+                    "Failed to update __migration_history table for {}",
+                    &migration.path.display(),
+                )
+            })?;
     }
+
     client.batch_execute("COMMIT")?;
     Ok(())
 }
--- src/migrate.rs
+++ src/migrate.rs
@@ -7,6 +7,39 @@ use std::{
 pub struct Migration {
     pub sql: String,
     pub path: PathBuf,
+    pub version_number: i32,
+}
+
+impl Migration {
+    pub fn from_file(path: PathBuf) -> Result<Migration> {
+        let sql = fs::read_to_string(&path)
+            .with_context(|| format!("Failed to read sql for {}", &path.display()))?;
+
+        let version_number = path
+            .file_name()
+            .with_context(|| format!("Failed to read filename for {}", &path.display()))?
+            .to_str()
+            .with_context(|| {
+                format!(
+                    "Failed to convert filename for {} to UTF-8",
+                    &path.display()
+                )
+            })?
+            .chars()
+            .nth(1)
+            .with_context(|| {
+                format!("Failed to extract the version number from the migration file")
+            })?
+            .to_digit(10)
+            .with_context(|| "The second character of the filename is an invalid version number")?;
+
+        let migration = Migration {
+            path,
+            sql,
+            version_number: version_number as i32,
+        };
+        Ok(migration)
+    }
 }
 
 pub fn find_migrations<P: AsRef<Path>>(dir_path: P) -> Result<Vec<Migration>> {
@@ -18,10 +51,7 @@ pub fn find_migrations<P: AsRef<Path>>(dir_path: P) -> Result<Vec<Migration>> {
     for entry in entries {
         let entry = entry?;
         let path = entry.path();
-        let sql = fs::read_to_string(&path)
-            .with_context(|| format!("Failed to read sql for {}", &path.display()))?;
-
-        let migration = Migration { path, sql };
+        let migration = Migration::from_file(path)?;
         migrations.push(migration);
     }
 
