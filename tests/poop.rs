ea9efe60bf87b8d1e905f7ca12b7fbca45a90e27
Thanh Doan <tmdoan98@gmail.com>
2020-05-16 14:42:21 +0100

    Add test for incrementing version numbers in sql filenames

--- tests/poop.rs
+++ tests/poop.rs
@@ -1,5 +1,6 @@
 mod utils;
 use assert_cmd::Command;
+use std::fs::{self, File};
 use utils::setup_test_dir_with_config;
 
 #[test]
@@ -42,6 +43,30 @@ fn poop_creates_sql_file_at_dir_from_config() {
         .join("src")
         .join("migrations")
         .join("V1__initial.sql")
+        .exists();
+    assert!(is_sql_file_created);
+}
+
+#[test]
+fn poop_creates_sql_file_with_incremented_version_number_when_other_migrations_exist() {
+    let config = "";
+    let test_dir = setup_test_dir_with_config(config);
+    let migrations_dir_path = test_dir.path().join("migrations");
+    fs::create_dir_all(&migrations_dir_path).unwrap();
+    File::create(migrations_dir_path.join("V1__initial.sql")).unwrap();
+    File::create(migrations_dir_path.join("V2__create_cars_table.sql")).unwrap();
+
+    let mut cmd = Command::cargo_bin("seagull").unwrap();
+    cmd.arg("poop")
+        .arg("create_users_table")
+        .current_dir(test_dir.path())
+        .assert()
+        .success();
+
+    let is_sql_file_created = test_dir
+        .path()
+        .join("migrations")
+        .join("V3__create_users_table.sql")
         .exists();
     assert!(is_sql_file_created);
 }
